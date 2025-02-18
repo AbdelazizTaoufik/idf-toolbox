208285330f0fd3ef9bb5591d9bd781aaa5f501a3
AbdelazizTaoufik <abdel-aziz.taoufik@web.de>
2025-02-18 20:55:22 +0100

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        @vite(['resources/css/app.css','resources/js/app.js'])
+    </head>
+    <body class="antialiased">
+        <p class="text-3xl font-bold color- hover:underline">
+            Hello World!
+        </p>
+    </body>
+</html>
