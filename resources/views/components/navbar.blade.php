1a837d0952bdffeafefb9f32f5cc4fbc59b05cc2
AbdelazizTaoufik <abdel-aziz.taoufik@web.de>
2025-02-19 23:08:14 +0100

    send submission

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-red-900 border-gray-200 dark:bg-gray-900">
+<nav class="bg-red-900 border-gray-200">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <a href="https://islamischedenkfabrik.de/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="{{ asset("storage/images/idf-logo.png") }}" class="h-16 rounded-lg transiontion duration-300 ease-in-out hover:scale-110 active:scale-90" alt="Islamische Denkfabrik Logo" />
