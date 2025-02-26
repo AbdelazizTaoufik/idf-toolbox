65bccdfd79daadc8825d955ab922494938aeeefa
AbdelazizTaoufik <abdel-aziz.taoufik@web.de>
2025-02-26 22:27:27 +0100

    qip

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-red-900 border-gray-200">
+<nav class="bg-red-900 border-gray-200 m-4 rounded-lg">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <a href="https://islamischedenkfabrik.de/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="{{ asset("storage/images/idf-logo.png") }}" class="h-16 rounded-lg transiontion duration-300 ease-in-out hover:scale-110 active:scale-90" alt="Islamische Denkfabrik Logo" />
--- resources/views/create-submission.blade.php
+++ resources/views/create-submission.blade.php
@@ -8,28 +8,30 @@
     </head>
     <body class="bg-gradient-to-t from-white via-red-200 to-white dark:bg-gradient-to-t dark:from-neutral-900 dark:via-red-900 dark:to-neutral-900">
         <x-navbar/>
-        <div class="bg-white dark:bg-red-950 relative overflow-x-auto shadow-md rounded-xl max-w-screen-xl mx-auto my-8">
-            <form id="submissionForm" action="{{ route('store.submission') }}" method="POST" class="p-4 md:p-5">
-                @csrf
-                <div class="grid gap-4 mb-4 grid-cols-2">
-                    <div class="col-span-2">
-                        <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Betreff</label>
-                        <input type="text" name="title" id="title" class="bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 border-red-300 focus:ring-red-500 focus:border-red-500 dark:bg-idfDarkRed dark:border-red-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+        <main class="flex-grow flex items-center justify-center p-4">
+            <div class="bg-white dark:bg-red-950 relative overflow-hidden shadow-lg rounded-2xl max-w-md w-full text-center">
+                <form id="submissionForm" action="{{ route('store.submission') }}" method="POST" class="p-4 md:p-5">
+                    @csrf
+                    <div class="grid gap-4 mb-4 grid-cols-2">
+                        <div class="col-span-2">
+                            <label for="title" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Betreff</label>
+                            <input type="text" name="title" id="title" class="bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 border-red-300 focus:ring-red-500 focus:border-red-500 dark:bg-idfDarkRed dark:border-red-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
+                        </div>
+                        <div class="col-span-2">
+                            <label for="text" class="text-left block mb-2 text-sm font-medium text-gray-900 dark:text-white">Inhalt</label>
+                            <textarea name="text" id="text" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-red-300 focus:ring-red-500 focus:border-red-500 dark:bg-idfDarkRed dark:border-red-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" required></textarea>
+                        </div>
                     </div>
-                    <div class="col-span-2">
-                        <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Inhalt</label>
-                        <textarea name="text" id="text" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-red-300 focus:ring-red-500 focus:border-red-500 dark:bg-idfDarkRed dark:border-red-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" required></textarea>
-                    </div>
-                </div>
-                <button type="submit" class="text-white inline-flex items-center bg-red-900 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                <svg class="w-6 h-6 pr-2  text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                    <path fill-rule="evenodd" d="M18 14a1 1 0 1 0-2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 1 0 0-2h-2v-2Z" clip-rule="evenodd"/>
-                    <path fill-rule="evenodd" d="M15.026 21.534A9.994 9.994 0 0 1 12 22C6.477 22 2 17.523 2 12S6.477 2 12 2c2.51 0 4.802.924 6.558 2.45l-7.635 7.636L7.707 8.87a1 1 0 0 0-1.414 1.414l3.923 3.923a1 1 0 0 0 1.414 0l8.3-8.3A9.956 9.956 0 0 1 22 12a9.994 9.994 0 0 1-.466 3.026A2.49 2.49 0 0 0 20 14.5h-.5V14a2.5 2.5 0 0 0-5 0v.5H14a2.5 2.5 0 0 0 0 5h.5v.5c0 .578.196 1.11.526 1.534Z" clip-rule="evenodd"/>
-                </svg>     
-                Absenden
-                </button>
-            </form>
-        </div>
+                    <button type="submit" class="text-white inline-flex items-center bg-red-900 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                    <svg class="w-6 h-6 pr-2  text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M18 14a1 1 0 1 0-2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 1 0 2 0v-2h2a1 1 0 1 0 0-2h-2v-2Z" clip-rule="evenodd"/>
+                        <path fill-rule="evenodd" d="M15.026 21.534A9.994 9.994 0 0 1 12 22C6.477 22 2 17.523 2 12S6.477 2 12 2c2.51 0 4.802.924 6.558 2.45l-7.635 7.636L7.707 8.87a1 1 0 0 0-1.414 1.414l3.923 3.923a1 1 0 0 0 1.414 0l8.3-8.3A9.956 9.956 0 0 1 22 12a9.994 9.994 0 0 1-.466 3.026A2.49 2.49 0 0 0 20 14.5h-.5V14a2.5 2.5 0 0 0-5 0v.5H14a2.5 2.5 0 0 0 0 5h.5v.5c0 .578.196 1.11.526 1.534Z" clip-rule="evenodd"/>
+                    </svg>     
+                    Absenden
+                    </button>
+                </form>
+            </div>
+        </main>
         <x-footer/>
     </body>
 </html>
